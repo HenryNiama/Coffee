7b34abe379d0b4ae20da552a82aa41c94675cae7
Henry Niama <hsniama@utpl.edu.ec>
2023-02-17 17:22:35 -0500

    Instalacion de React Router Dom  y creacion de layouts

---
+++ coffee-react-frontend/src/router.jsx
@@ -0,0 +1,8 @@
+import {createBrowserRouter} from 'react-router-dom'
+import Layout from './layouts/layout'
+
+const router = createBrowserRouter([
+    { path: '/', element: <Layout /> },
+]);
+
+export default router;
