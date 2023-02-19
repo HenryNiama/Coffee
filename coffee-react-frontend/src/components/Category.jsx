f1bf8c443a7252ca514e1da251410850745ebc62
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 15:29:50 -0500

    Creacion de menu de categorias y configuracion de Sidebar.jsx

---
+++ coffee-react-frontend/src/components/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Category({category}) {
+
+    const {id, icono, nombre} = category;
+
+    return (
+        <div className={"flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer"}>
+            <img src={`/img/icono_${icono}.svg`} alt="Imagen Icono" className={"w-12"}/>
+            <p className={"text-lg font-bold cursor-pointer truncate"}>{nombre}</p>
+        </div>
+    );
+}
+
+export default Category;
--- coffee-react-frontend/src/components/Sidebar.jsx
+++ coffee-react-frontend/src/components/Sidebar.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {categories} from "../data/categories.js";
+import Category from "./Category.jsx";
 
 function Sidebar() {
     return (
         <aside className={"md:w-72"}>
             <div className={"p-4"}>
-                <img src="../../public/img/logo.svg" alt="" className={"w-40"}/>
+                <img src="../../public/img/logo.svg" alt="Image Logo" className={"w-40"}/>
             </div>
             <div className={"mt-10"}>
                 {categories.map(category => (
-                    <p>{category.nombre}</p>
+                    <Category
+                        category={category}
+                    />
                 )) }
+            </div>
+            <div className={"my-5 px-5"}>
+                <button type={"button"} className={"text-center bg-red-500 w-full p-3 font-bold text-white hover:bg-red-700"}>
+                    Cancel Order
+                </button>
             </div>
         </aside>
     );
