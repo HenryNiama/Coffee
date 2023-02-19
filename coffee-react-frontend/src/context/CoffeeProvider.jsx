45b9a56824649d725f14628bb6c52a4d41d24774
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 17:11:41 -0500

    Creando un Context para manejar el estado de la App.

---
+++ coffee-react-frontend/src/context/CoffeeProvider.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createContext} from 'react';
+
+
+
+const CoffeeContext = createContext();
+
+const CoffeeProvider = ({children}) => {
+    return (
+        <CoffeeContext.Provider
+            value={{
+
+            }}>
+            {children}
+        </CoffeeContext.Provider>
+    );
+};
+
+export {CoffeeProvider};
+export default CoffeeContext;
--- coffee-react-frontend/src/main.jsx
+++ coffee-react-frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider} from 'react-router-dom'
+import { CoffeeProvider} from "./context/CoffeeProvider.jsx";
 import router from './router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <CoffeeProvider>
+          <RouterProvider router={router} />
+      </CoffeeProvider>
   </React.StrictMode>,
 )
