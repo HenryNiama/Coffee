1b8cffcb2e32c87f4d2c986a6ab907eae034fba0
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 14:40:46 -0500

    Obteniendo y mostrando las categorias

--- coffee-react-frontend/src/components/Sidebar.jsx
+++ coffee-react-frontend/src/components/Sidebar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {categories} from "../data/categories.js";
 
 function Sidebar() {
     return (
-        <div className={"md:w-72"}>Sidebar </div>
+        <aside className={"md:w-72"}>
+            <div className={"p-4"}>
+                <img src="../../public/img/logo.svg" alt="" className={"w-40"}/>
+            </div>
+            <div className={"mt-10"}>
+                {categories.map(category => (
+                    <p>{category.nombre}</p>
+                )) }
+            </div>
+        </aside>
     );
 }
 
