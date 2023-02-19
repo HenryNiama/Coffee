5382456851f27036d005c6d3edc104b17cc26d28
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 13:30:47 -0500

    Primeros pasos con el formulario de registro

--- coffee-react-frontend/src/views/Register.jsx
+++ coffee-react-frontend/src/views/Register.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-function Register(props) {
+function Register() {
     return (
-        <div>Registro</div>
+        <>
+            <h1 className="text-4xl font-black">Create your Account</h1>
+            <p>Fill the form</p>
+
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form action="">
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name: </label>
+                        <input type="text" name="name" id="name" placeholder="Your Name"
+                               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                </form>
+            </div>
+        </>
+
     );
 }
 
