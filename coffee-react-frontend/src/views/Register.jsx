3c7ef37ba2677571b7f60f4a54f02dd3349ce6e6
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 13:37:43 -0500

    Finalizando el formulario de registro.

--- coffee-react-frontend/src/views/Register.jsx
+++ coffee-react-frontend/src/views/Register.jsx
@@ -14,6 +14,27 @@ function Register() {
                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         />
                     </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email: </label>
+                        <input type="email" name="email" id="email" placeholder="Your Email"
+                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password: </label>
+                        <input type="password" name="password" id="password" placeholder="Your Password"
+                               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password_confirmation" className="block text-gray-700 text-sm font-bold mb-2">Confirm Password: </label>
+                        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password"
+                               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <input type="submit" value="Create Account" className={"bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"} />
+
+
                 </form>
             </div>
         </>
