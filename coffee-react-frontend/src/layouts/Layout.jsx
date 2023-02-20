44711e3fdfb7d97b31674edc5fc6a3ba5d46208c
Henry Niama <hsniama@utpl.edu.ec>
2023-02-19 19:24:31 -0500

    Instalando y Configurando React Modal

--- coffee-react-frontend/src/layouts/Layout.jsx
+++ coffee-react-frontend/src/layouts/Layout.jsx
@@ -3,12 +3,25 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from "../components/Sidebar.jsx";
 import Summary from "../components/Summary.jsx";
 import useCoffee from "../hooks/useCoffee.js";
+import Modal from "react-modal";
+
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
 
 export default function layout() {
 
-    const {modal} = useCoffee();
+    const {modal, handleClickModal} = useCoffee();
 
   return (
+      <>
       <div className="md:flex">
           <Sidebar />
           <main className={"flex-1 h-screen overflow-y-scroll bg-gray-100 p-3"}>
@@ -16,5 +29,18 @@ export default function layout() {
           </main>
           <Summary />
       </div>
+
+          {modal && (
+                <Modal
+                    isOpen={modal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2>Modal</h2>
+                    <button onClick={handleClickModal}>Close</button>
+                    <div>I am a modal</div>
+                </Modal>
+            )}
+      </>
   )
 }
