31e2447d691810f5732b541a4b77407f8d1b371a
Henry Niama <hsniama@utpl.edu.ec>
2023-02-21 12:23:36 -0500

    - Creando una respuesta JSON para las Categorias. - Crendo un API Resource para Categorias.

--- coffee-laravel-backend/app/Http/Controllers/CategoryController.php
+++ coffee-laravel-backend/app/Http/Controllers/CategoryController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryCollection;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): CategoryCollection
+    {
+        //return response()->json(['categories' => Category::all()]);
+        return new CategoryCollection(Category::all());
+    }
 }
