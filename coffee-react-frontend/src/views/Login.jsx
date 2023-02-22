6c2eb612ab804935fb0bc2837ebea4a6bbc22e35
Henry Niama <hsniama@utpl.edu.ec>
2023-02-22 12:46:30 -0500

    Validando la Autenticación en React

--- coffee-react-frontend/src/views/Login.jsx
+++ coffee-react-frontend/src/views/Login.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {createRef, useState} from "react";
+import clientAxios from "../config/axios.js";
+import Alert from "../components/Alert.jsx";
 
 function Login() {
+
+    const emailRef = createRef();
+    const passwordRef = createRef();
+
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        console.log(data);
+
+        try {
+            const response = await clientAxios.post('/api/login', data);
+            console.log(response.data.token);
+        } catch (error) {
+            console.log(Object.values(error.response.data.errors));
+            setErrors(Object.values(error.response.data.errors));
+        }
+    }
+
     return (
         <>
             <h1 className="text-4xl font-black">Sign In</h1>
             <p className="text-lg text-gray-600"> Write your credentials. </p>
 
             <div className="bg-white shadow-md rounded-md mt-6 px-5 py-6">
-                <form action="">
+                <form action="" onSubmit={handleSubmit} noValidate>
+
+                    {errors ? errors.map((error, i) => <Alert key={i}>{error}</Alert>) : null}
+
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email: </label>
                         <input type="email" name="email" id="email" placeholder="Your Email"
                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                               ref={emailRef}
                         />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password: </label>
                         <input type="password" name="password" id="password" placeholder="Your Password"
                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                               ref={passwordRef}
                         />
                     </div>
                     <input type="submit" value="Log In" className={"bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"} />
