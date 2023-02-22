f0bfc0445efedb363f1b427ed7095f9f6a4d2692
Henry Niama <hsniama@utpl.edu.ec>
2023-02-22 11:46:51 -0500

    configurando los headers el Axios.js

--- coffee-react-frontend/src/config/axios.js
+++ coffee-react-frontend/src/config/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Accept" : "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+    }, withCredentials: true
 })
 
 export default clientAxios;
