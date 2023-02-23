d3cf0b8030eff0d0544f404ee342b827fd3a96ba
Henry Niama <hsniama@utpl.edu.ec>
2023-02-22 20:08:57 -0500

    - Guardando el Token de Inicio de Sesion en localStorage.

--- coffee-react-frontend/src/views/Login.jsx
+++ coffee-react-frontend/src/views/Login.jsx
@@ -21,7 +21,9 @@ function Login() {
 
         try {
             const response = await clientAxios.post('/api/login', data);
-            console.log(response.data.token);
+            // console.log(response.data.token);
+            localStorage.setItem('AUTH_TOKEN', response.data.token);
+            setErrors([]);
         } catch (error) {
             console.log(Object.values(error.response.data.errors));
             setErrors(Object.values(error.response.data.errors));
