15903bfbc63a25c0c80722fb711581aaf1b2eff2
Henry Niama <hsniama@utpl.edu.ec>
2023-02-24 10:15:26 -0500

    - Consultar las Ordenes

--- coffee-react-frontend/src/views/Orders.jsx
+++ coffee-react-frontend/src/views/Orders.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import useSWR from "swr";
+import clientAxios from "../config/axios.js";
+
 
 function Orders(props) {
+
+    const token = localStorage.getItem('AUTH_TOKEN');
+
+    const fetcher = () => clientAxios('/api/orders', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    const {data, error, isLoading} = useSWR('/api/orders', fetcher);
+
+    console.log(data);
+    console.log(error);
+    console.log(isLoading);
+
+
     return (
         <div className={"w-3/5 px-1"}>
             <h1 className={"text-4xl font-black text-center"}>Orders</h1>
